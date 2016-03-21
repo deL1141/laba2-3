f8501c6fe15d1f0bacfaafbdf0c2eaa2da70cbcf
Дмитрий <vancor86>
2016-03-21 23:10:34 +0500

    Третье задание

---
+++ src/zadanie3.java
@@ -0,0 +1,14 @@
+/**
+ * Created by deL on 21.03.2016.
+ */
+public class zadanie3 {
+    public static void main(String[] args){
+        int a=10,b=10;
+        int k=1;
+        String c="8";
+        for(k=1;k<=a;k++){
+            System.out.println(c);
+            c=c+"8";
+        }
+    }
+}
